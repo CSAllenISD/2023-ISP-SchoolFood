237d07ccc13441420060238ebf0ccffd09af9d6b
brett-k-cs <bkapla01@gmail.com>
2023-02-10 10:22:34 -0600

    Fixed bindings

--- App/app/src/main/java/com/schoolfood/ui/previousordered/PreviousOrderedFragment.kt
+++ App/app/src/main/java/com/schoolfood/ui/previousordered/PreviousOrderedFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.schoolfood.databinding.FragmentNotificationsBinding
+import com.schoolfood.databinding.FragmentPreviousorderedBinding
 
 class PreviousOrderedFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentPreviousorderedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class PreviousOrderedFragment : Fragment() {
         val previousOrderedViewModel =
                 ViewModelProvider(this).get(PreviousOrderedViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentPreviousorderedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- App/app/src/main/java/com/schoolfood/ui/profile/ProfileFragment.kt
+++ App/app/src/main/java/com/schoolfood/ui/profile/ProfileFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.schoolfood.databinding.FragmentDashboardBinding
+import com.schoolfood.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class ProfileFragment : Fragment() {
         val profileViewModel =
                 ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
