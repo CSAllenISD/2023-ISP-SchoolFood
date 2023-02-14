7200b7dc033cb7dfb810751f122732bb088f742d
captainbboy <bkapla01@gmail.com>
2023-02-14 16:27:56 -0600

    Added login activity + made profile look better

---
+++ App/app/src/main/java/com/schoolfood/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.schoolfood
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.schoolfood.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val signInBtn = binding.signIn
+        signInBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
--- App/app/src/main/java/com/schoolfood/MainActivity.kt
+++ App/app/src/main/java/com/schoolfood/MainActivity.kt
@@ -22,11 +22,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_prevordered))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
