db4999febfd4cd77b4eaa584cbbefd32c57d622b
brett-k-cs <bkapla01@gmail.com>
2023-04-05 10:35:19 -0500

    added contact page

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,11 +6,26 @@ router.get('/', function(req, res, next) {
   console.log("REQUESTED /");
   
   try {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'SchoolFood' });
   } catch (err) {
     console.error(err)
     next(err)
   }
+});
+
+router.get('/preorder', function(req, res, next) {
+    res.render('preorder', { title: 'PreOrder' });
+});
+router.get('/order', function(req, res, next) {
+  res.render('preorder', { title: 'PreOrder' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'PreOrder' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'PreOrder' });
 });
 
 module.exports = router;
